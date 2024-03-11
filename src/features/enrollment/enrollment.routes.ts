cebab79ac8658a2c2b429b8b01cd000dc3b9ddc6
Amr Khaled <lewolfje@gmail.com>
2024-03-11 16:42:51 +0200

    Add enrollment routes

---
+++ src/features/enrollment/enrollment.routes.ts
@@ -0,0 +1,26 @@
+import { Router } from "express";
+
+import { asyncHandler } from "@fcai-sis/shared-utilities";
+import getPassedCoursesMiddleware from "./logic/middlewares/getPassedCourses.middleware";
+import enrollCourseHandler from "./logic/handlers/enrollCourse.handler";
+import fetchEligibleCourses from "./logic/handlers/fetchEligibleCourses.handler";
+import createEnrollment from "./logic/handlers/createEnrollment.handler";
+import coursePassedHandler from "./logic/handlers/CoursePassed.handler";
+
+export default (router: Router) => {
+  router.post("/create", asyncHandler(createEnrollment));
+
+  router.post(
+    "/enroll/:studentId",
+    getPassedCoursesMiddleware,
+    asyncHandler(enrollCourseHandler)
+  );
+
+  router.get(
+    "/courses/:studentId",
+    getPassedCoursesMiddleware,
+    asyncHandler(fetchEligibleCourses)
+  );
+
+  router.post("/pass", asyncHandler(coursePassedHandler));
+};
