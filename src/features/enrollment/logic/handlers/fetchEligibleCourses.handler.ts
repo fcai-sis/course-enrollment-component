8fa8c354cc8bceeb65537eb62f8c50bc780c7c80
Amr Khaled <lewolfje@gmail.com>
2024-03-11 16:43:27 +0200

    WIP

---
+++ src/features/enrollment/logic/handlers/fetchEligibleCourses.handler.ts
@@ -0,0 +1,60 @@
+import { Request, Response } from "express";
+import { CourseEnrollmentModel } from "../../data/models/enrollment.model";
+import { CourseModel } from "@fcai-sis/shared-models";
+
+/**
+ * Fetch all courses that a student is eligible to enroll in
+ */
+
+type HandlerRequest = Request<
+  {
+    studentId: string;
+  },
+  {},
+  {}
+>;
+
+const handler = async (req: HandlerRequest, res: Response) => {
+  const { passedCourses } = req as any;
+  const studentId = req.params.studentId;
+  // console.log(passedCourses);
+
+  const enrolledCourses = await CourseEnrollmentModel.find({
+    student: studentId,
+  }).populate("courses");
+
+  if (!enrolledCourses || !enrolledCourses.length) {
+    return res.status(404).json({ message: "No courses found" });
+  }
+
+  const courses = await CourseModel.find({});
+  // TODO : filter out the courses that are already enrolled in by this user
+  const availableCourses = courses.filter((course) => {
+    return !enrolledCourses.some((enrollment) =>
+      enrollment.courses.some(
+        (enrolledCourse) => enrolledCourse._id === course._id
+      )
+    );
+  });
+
+  // Using the passed courses array, if a prerequisite ID is not found in the passed courses array, remove the course from the available courses
+  const filteredAvailableCourses = availableCourses.filter((course) => {
+    if (course.prerequisites) {
+      return course.prerequisites.every((prerequisite) =>
+        passedCourses.includes(prerequisite)
+      );
+    }
+    return true;
+  });
+
+  const response = {
+    studentId,
+    courses: enrolledCourses.map((enrollment) => enrollment.courses),
+    availableCourses: filteredAvailableCourses,
+  };
+
+  return res.status(200).json(response);
+};
+
+const fetchEligibleCourses = handler;
+export default fetchEligibleCourses;
