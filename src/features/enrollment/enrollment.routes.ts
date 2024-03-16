f161b362224216d565541d89b5c469ff5e1ab706
Amr Khaled <lewolfje@gmail.com>
2024-03-16 20:00:30 +0200

    add fetch enrolled course route and remove passcourse route

--- src/features/enrollment/enrollment.routes.ts
+++ src/features/enrollment/enrollment.routes.ts
@@ -1,20 +1,20 @@
 import { Router } from "express";
 
 import { asyncHandler } from "@fcai-sis/shared-utilities";
-import coursePassedHandler from "./logic/handlers/passCourse.handler";
 import enrollCourseHandler from "./logic/handlers/enrollCourse.handler";
 import createEnrollmentHandler from "./logic/handlers/createEnrollment.handler";
 import fetchEligibleCourses from "./logic/handlers/fetchEligibleCourses.handler";
 import getPassedCoursesMiddleware from "./logic/middlewares/getPassedCourses.middleware";
 import ensureEnrollmentExistsMiddleware from "./logic/middlewares/ensureEnrollmentExists.middleware";
 import validateCreateEnrollmentRequestMiddleware from "./logic/middlewares/validateCreateEnrollmentRequest.middleware";
+import fetchEnrolledCourses from "./logic/handlers/fetchEnrolledCourses.handler";
 
 export default (router: Router) => {
   router.post(
     "/create",
     validateCreateEnrollmentRequestMiddleware,
     ensureEnrollmentExistsMiddleware,
-    asyncHandler(createEnrollmentHandler),
+    asyncHandler(createEnrollmentHandler)
   );
 
   router.post(
@@ -29,8 +29,5 @@ export default (router: Router) => {
     asyncHandler(fetchEligibleCourses)
   );
 
-  router.post(
-    "/pass",
-    asyncHandler(coursePassedHandler),
-  );
+  router.get("/enrolled/:studentId", asyncHandler(fetchEnrolledCourses));
 };
