0c4fd59653e1a55409058eccd67d7adbf008104a
Amr Khaled <lewolfje@gmail.com>
2024-03-16 21:02:20 +0200

    use course code instead of _id, change validation error response

--- src/features/enrollment/logic/middlewares/validateCreateEnrollmentRequest.middleware.ts
+++ src/features/enrollment/logic/middlewares/validateCreateEnrollmentRequest.middleware.ts
@@ -1,20 +1,21 @@
-import * as validator from 'express-validator';
-import { Request, Response, NextFunction } from 'express';
-import { CourseModel, StudentModel } from '@fcai-sis/shared-models';
+import * as validator from "express-validator";
+import { Request, Response, NextFunction } from "express";
+import { CourseModel, StudentModel } from "@fcai-sis/shared-models";
+import logger from "../../../../core/logger";
 
 const validateCreateEnrollmentRequestMiddleware = [
   validator
-    .body('studentId')
+    .body("studentId")
     .exists()
-    .withMessage('Student ID is required')
+    .withMessage("Student ID is required")
     .isMongoId()
-    .withMessage('Invalid student ID')
+    .withMessage("Invalid student ID")
     .custom(async (value, { req }) => {
       // Fetch the student
       const student = await StudentModel.findById(value);
 
       if (!student) {
-        throw new Error('Student not found');
+        throw new Error("Student not found");
       }
 
       req.body.student = student;
@@ -23,11 +24,11 @@ const validateCreateEnrollmentRequestMiddleware = [
     }),
 
   validator
-    .body('courses')
+    .body("courses")
     .exists()
-    .withMessage('Courses to enroll in is required')
+    .withMessage("Courses to enroll in is required")
     .isArray()
-    .withMessage('At least one course is required')
+    .withMessage("At least one course is required")
     .custom((_, { req }) => {
       req.body.coursesToEnrollIn = [];
 
@@ -35,15 +36,25 @@ const validateCreateEnrollmentRequestMiddleware = [
     }),
 
   validator
-    .body('courses.*')
-    .isMongoId()
-    .withMessage('Invalid course ID')
+    .body("courses.*")
+    .custom((value) => {
+      // Course code must follow this pattern: 2-4 uppercase letters followed by 3 digits
+      const pattern = /^[A-Z]{2,4}\d{3}$/;
+      if (!pattern.test(value)) {
+        throw new Error(
+          "Invalid course code, must be 2-4 uppercase letters followed by 3 digits"
+        );
+      }
+
+      return true;
+    })
+    .withMessage("Invalid course code")
     .custom(async (value, { req }) => {
       // Fetch the course
-      const course = await CourseModel.findById(value);
+      const course = await CourseModel.findOne({ code: value });
 
       if (!course) {
-        throw new Error('Course not found');
+        throw new Error("Course not found");
       }
 
       req.body.coursesToEnrollIn.push(course);
@@ -52,10 +63,23 @@ const validateCreateEnrollmentRequestMiddleware = [
     }),
 
   (req: Request, res: Response, next: NextFunction) => {
+    logger.debug(
+      `Validating create enrollment req body: ${JSON.stringify(req.body)}`
+    );
     const errors = validator.validationResult(req);
 
     if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
+      logger.debug(
+        `Validation failed for create enrollment req body: ${JSON.stringify(
+          req.body
+        )}`
+      );
+
+      return res.status(400).json({
+        error: {
+          message: errors.array()[0].msg,
+        },
+      });
     }
 
     next();
