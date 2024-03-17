3f7bff7e9f580fa5712cd7011d9cdc6d95de1bfd
Amr Khaled <lewolfje@gmail.com>
2024-03-17 17:27:33 +0200

    rename

---
+++ src/features/enrollment/logic/middlewares/validateEnrollment.middleware.ts
@@ -0,0 +1,45 @@
+import { Document } from "mongoose";
+import { Request, Response, NextFunction } from "express";
+import { CourseType, StudentType } from "@fcai-sis/shared-models";
+
+import {
+  EnrollmentModel,
+  EnrollmentType,
+} from "../../data/models/enrollment.model";
+
+type MiddlewareRequest = Request<
+  {},
+  {},
+  {
+    studentId: string;
+    enrollment: EnrollmentType & Document;
+    coursesToEnrollIn: (CourseType & Document)[];
+    student: StudentType & Document;
+  }
+>;
+
+const validateEnrollmentMiddleware = async (
+  req: MiddlewareRequest,
+  _: Response,
+  next: NextFunction
+) => {
+  const { studentId } = req.body;
+
+  // Get the student's enrollment object if it exists
+  const existingEnrollment = await EnrollmentModel.findOne({ studentId });
+
+  // If the enrollment doesn't exist, create a new one
+  // Otherwise, add the new courses to the existing enrollment
+  if (!existingEnrollment) {
+    req.body.enrollment = await EnrollmentModel.create({
+      studentId,
+      courses: [],
+    });
+  } else {
+    req.body.enrollment = existingEnrollment;
+  }
+
+  next();
+};
+
+export default validateEnrollmentMiddleware;
