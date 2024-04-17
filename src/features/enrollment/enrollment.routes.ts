cf788dfba87310f19afefd97899f0f7c35cd2c64
Amr Khaled <lewolfje@gmail.com>
2024-04-17 15:10:13 +0200

    add passedCourses middleware to /create endpoint

--- src/features/enrollment/enrollment.routes.ts
+++ src/features/enrollment/enrollment.routes.ts
@@ -16,6 +16,7 @@ export default (router: Router) => {
     "/create",
     validateCreateEnrollmentRequestMiddleware,
     validateEnrollmentMiddleware,
+    getPassedCoursesMiddleware,
     asyncHandler(createEnrollmentHandler)
   );
 
