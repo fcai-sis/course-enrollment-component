e40dce37cf2a7b5d5d83070e12f6bae26c3f4356
Amr Khaled <lewolfje@gmail.com>
2024-04-20 14:22:12 +0200

    Add createGraduationGroup handler

---
+++ src/features/graduation/logic/handlers/createGraduationGroup.handler.ts
@@ -0,0 +1,42 @@
+import mongoose from "mongoose";
+import { Request, Response } from "express";
+import GraduationProjectTeamModel from "../../data/models/gpt.model";
+import { EnrollmentType } from "../../../enrollment/data/models/enrollment.model";
+
+type HandlerRequest = Request<
+  {},
+  {},
+  {
+    enrollments: (EnrollmentType & Document)[];
+    instructorTeachings: mongoose.Schema.Types.ObjectId[];
+    assistantTeachings: mongoose.Schema.Types.ObjectId[];
+    semester: mongoose.Schema.Types.ObjectId;
+  }
+>;
+
+/*
+ * Creates a graduation project group.
+ * */
+const handler = async (req: HandlerRequest, res: Response) => {
+  const { enrollments, instructorTeachings, assistantTeachings, semester } =
+    req.body;
+
+  const graduationProject = new GraduationProjectTeamModel({
+    enrollments,
+    instructorTeachings,
+    assistantTeachings,
+    semester,
+  });
+
+  await graduationProject.save();
+
+  const response = {
+    message: "Graduation project group created successfully",
+    graduationProject,
+  };
+
+  return res.status(201).json(response);
+};
+
+const CreateGraduationGroupHandler = handler;
+export default CreateGraduationGroupHandler;
