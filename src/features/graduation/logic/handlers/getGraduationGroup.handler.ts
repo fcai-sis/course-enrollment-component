e769c257652688dec081db957eb5912851473df2
Amr Khaled <lewolfje@gmail.com>
2024-04-20 14:22:28 +0200

    Add getGraduationGroup handler

---
+++ src/features/graduation/logic/handlers/getGraduationGroup.handler.ts
@@ -0,0 +1,31 @@
+import { Request, Response } from "express";
+import GraduationProjectTeamModel from "../../data/models/gpt.model";
+
+type HandlerRequest = Request<
+  {
+    groupId: string;
+  },
+  {},
+  {}
+>;
+
+/**
+ * Gets a graduation group by its group ID
+ */
+const handler = async (req: HandlerRequest, res: Response) => {
+  const { groupId } = req.params;
+
+  const graduationGroup = await GraduationProjectTeamModel.findOne({ groupId });
+
+  if (!graduationGroup) {
+    return res.status(404).json({
+      message: "Graduation group not found",
+    });
+  }
+
+  return res.status(200).json(graduationGroup);
+};
+
+const getGraduationGroupHandler = handler;
+
+export default getGraduationGroupHandler;
