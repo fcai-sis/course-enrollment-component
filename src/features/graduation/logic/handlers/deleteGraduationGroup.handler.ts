31d7edef4c9219c5312f2ea47bbd695cd392ddb3
Amr Khaled <lewolfje@gmail.com>
2024-04-20 14:22:17 +0200

    Add deleteGraduationGroup handler

---
+++ src/features/graduation/logic/handlers/deleteGraduationGroup.handler.ts
@@ -0,0 +1,39 @@
+import { Request, Response } from "express";
+import { GraduationProjectTeamModel } from "../../data/models/gpt.model";
+
+type HandlerRequest = Request<
+  {
+    groupId: string;
+  },
+  {},
+  {}
+>;
+
+/**
+ * Deletes a graduation group by its group ID
+ * */
+
+const handler = async (req: HandlerRequest, res: Response) => {
+  const { groupId } = req.params;
+
+  const graduationGroup = await GraduationProjectTeamModel.findByIdAndDelete(
+    groupId
+  );
+
+  if (!graduationGroup) {
+    return res.status(404).json({
+      message: "Graduation group not found",
+    });
+  }
+
+  const response = {
+    message: "Graduation group deleted successfully",
+    graduationGroup,
+  };
+
+  return res.status(200).json(response);
+};
+
+const deleteGraduationGroupHandler = handler;
+
+export default deleteGraduationGroupHandler;
