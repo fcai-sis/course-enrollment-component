87134335a2697ed377faa0326eaa41aa936cd5e5
Amr Khaled <lewolfje@gmail.com>
2024-05-22 21:33:38 +0300

    turn response into error obj

--- src/features/enrollment/logic/middlewares/getPassedCourses.middleware.ts
+++ src/features/enrollment/logic/middlewares/getPassedCourses.middleware.ts
@@ -1,14 +1,23 @@
 import { StudentModel } from "@fcai-sis/shared-models";
 import { Request, Response, NextFunction } from "express";
 
-import { EnrollmentModel } from "../../data/models/enrollment.model";
+import { EnrollmentModel } from "@fcai-sis/shared-models";
 
 // Middleware chain
-const getPassedCoursesMiddleware = async (req: Request, res: Response, next: NextFunction) => {
+const getPassedCoursesMiddleware = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   const { userId } = req.body.user;
   const student = await StudentModel.findOne({ userId });
 
-  if (!student) return res.status(404).json({ message: "Student not found" });
+  if (!student)
+    return res.status(404).json({
+      error: {
+        message: "Student not found",
+      },
+    });
 
   const studentId = student._id;
   const passedCourses: any = [];
