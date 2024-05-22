0a72334311ea8a50d79f19d8b94a0ba51a64f86d
Amr Khaled <lewolfje@gmail.com>
2024-05-22 21:31:19 +0300

    fix filters not working, use shared model

--- src/features/enrollment/logic/handlers/fetchEligibleCourses.handler.ts
+++ src/features/enrollment/logic/handlers/fetchEligibleCourses.handler.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express";
 import { CourseModel, StudentModel } from "@fcai-sis/shared-models";
 
-import { EnrollmentModel } from "../../data/models/enrollment.model";
+import { EnrollmentModel } from "@fcai-sis/shared-models";
 import { FetchEligibleCoursesContextType } from "../contexts/fetchEligibleCourses.context";
 
 /**
@@ -13,7 +13,7 @@ const fetchEligibleCourses = async (req: Request, res: Response) => {
 
   if (!student) return res.status(404).json({ message: "Student not found" });
 
-  const studentId = student.studentId;
+  const studentId = student._id;
   const { passedCourses } = req.body as FetchEligibleCoursesContextType;
 
   // Find all enrollments with this student ID
@@ -21,22 +21,45 @@ const fetchEligibleCourses = async (req: Request, res: Response) => {
 
   // Find all courses that are available for enrollment
   const courses = await CourseModel.find({});
+
   let availableCourses;
 
   // If there's no enrollment, return all available courses
   if (enrollments.length === 0) {
     availableCourses = courses;
   } else {
-    // If there are enrollments, remove courses that the student has already enrolled in (any enrollment with a status of "enrolled" or "passed")
+    // If there are enrollments, get courses that the student has already enrolled in (any enrollment with a status of "enrolled" or "passed")
     const enrolledCourses = enrollments.filter((enrollment) => {
       return enrollment.status === "enrolled" || enrollment.status === "passed";
     });
-
-    availableCourses = courses.filter((course) => !enrolledCourses.some((enrollment) => enrollment.courseId === course._id));
+    // remove courses that the student has already enrolled in from the available courses
+    availableCourses = courses.filter(
+      (course) =>
+        !enrolledCourses.some(
+          (enrollment) =>
+            enrollment.courseId.toString() === course._id.toString()
+        )
+    );
   }
 
   // Using the passed courses array, if a prerequisite ID is not found in the passed courses array, remove the course from the available courses
-  const filteredAvailableCourses = availableCourses.filter((course) => course.prerequisites.every((prerequisite) => passedCourses.includes(prerequisite.toString())));
+  const filteredAvailableCourses = availableCourses.filter((course) =>
+    course.prerequisites.every((prerequisite: any) => {
+      console.log("PREREUISITES", prerequisite);
+      console.log("PASSED COURSES", passedCourses);
+      console.log(
+        "PASSED COURSES INCLUDES",
+        passedCourses
+          .map((id) => id.toString())
+          .includes(prerequisite.toString())
+      );
+
+      return passedCourses
+        .map((id) => id.toString())
+        .includes(prerequisite.toString());
+    })
+  );
+
 
   const response = {
     studentId,
