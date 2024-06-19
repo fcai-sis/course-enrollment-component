69a13751624b21d21747aad91419cb2ba9e6860a
Amr Khaled <lewolfje@gmail.com>
2024-06-19 18:59:25 +0300

    refactor to use semesterId and courseId during fetch

--- src/features/enrollment/logic/handlers/fetchAllSemesterEnrollments.handler.ts
+++ src/features/enrollment/logic/handlers/fetchAllSemesterEnrollments.handler.ts
@@ -3,24 +3,28 @@ import { Request, Response } from "express";
 import { EnrollmentModel } from "@fcai-sis/shared-models";
 
 type HandlerRequest = Request<
+  {},
+  {},
   {
     semesterId: string;
-  },
-  {},
-  {}
+    courseId: string;
+  }
 >;
 
 /**
- * Fetches all enrollments in a semester
+ * Fetches all enrollments in a semester for a specific course
  */
 const handler = async (req: HandlerRequest, res: Response) => {
-  const { semesterId } = req.params;
-  const enrollments = await EnrollmentModel.find({ semesterId });
+  const { semesterId, courseId } = req.body;
+  const enrollments = await EnrollmentModel.find({
+    semester: semesterId,
+    course: courseId,
+  });
 
   if (!enrollments) {
     return res.status(404).json({
       error: {
-        message: "No enrollments found",
+        message: "No enrollments found for this course in this semester",
       },
     });
   }
