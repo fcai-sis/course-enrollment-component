ccc4a145c7ce6a7661a93151871914bdbb4fbeb0
Amr Khaled <lewolfje@gmail.com>
2024-06-21 19:18:56 +0300

    refactor update enrollment to work with new stuff

--- src/features/enrollment/logic/middlewares/updateEnrollmentRequestBody.middleware.ts
+++ src/features/enrollment/logic/middlewares/updateEnrollmentRequestBody.middleware.ts
@@ -1,11 +1,6 @@
 import * as validator from "express-validator";
-import { Request, Response, NextFunction } from "express";
-import {
-  EnrollmentModel,
-  EnrollmentStatus,
-  Hall,
-} from "@fcai-sis/shared-models";
-import logger from "../../../../core/logger";
+import { EnrollmentStatusEnum, HallModel } from "@fcai-sis/shared-models";
+import { validateRequestMiddleware } from "@fcai-sis/shared-middlewares";
 
 /*
  * Middleware to validate the request body for updating an enrollment
@@ -17,63 +12,41 @@ const validateUpdateEnrollmentRequestBodyMiddleware = [
     .body("enrollment")
     .exists()
     .withMessage("Enrollment is required")
-    .custom(async (value, { req }) => {
-      // Ensure enrollment exists
-      const enrollment = await EnrollmentModel.findOne({ _id: value });
-
-      if (!enrollment) throw new Error("Enrollment not found");
-
-      return true;
-    }),
+    .isMongoId()
+    .withMessage("Invalid enrollment ID"),
 
   validator
     .body("status")
     .optional()
-    .isIn(["passed", "failed", "enrolled"] as EnrollmentStatus[])
+    .isIn(Object.values(EnrollmentStatusEnum))
     .withMessage("Invalid status"),
 
   validator
-    .body("seatNumber")
+    .body("exam")
+    .optional()
+    .isObject()
+    .withMessage("Exam must be an object"),
+
+  validator
+    .body("exam.hall")
+    .optional()
+    .isMongoId()
+    .withMessage("Invalid exam hall ID")
+    .custom(async (value) => {
+      // Ensure exam hall exists
+      const examHall = await HallModel.findOne({ _id: value });
+
+      if (!examHall) throw new Error("Hall not found");
+
+      return true;
+    }),
+  validator
+    .body("exam.seatNumber")
     .optional()
     .isNumeric()
     .withMessage("Seat number must be a number"),
 
-  validator
-    .body("examHall")
-    .optional()
-    .isMongoId()
-    .withMessage("Invalid exam hall ID")
-    .custom(async (value, { req }) => {
-      // Ensure exam hall exists
-      const examHall = await Hall.findOne({ _id: value });
-
-      if (!examHall) throw new Error("Exam hall not found");
-
-      return true;
-    }),
-
-  (req: Request, res: Response, next: NextFunction) => {
-    logger.debug(
-      `Validating create enrollment req body: ${JSON.stringify(req.body)}`
-    );
-    const errors = validator.validationResult(req);
-
-    if (!errors.isEmpty()) {
-      logger.debug(
-        `Validation failed for create enrollment req body: ${JSON.stringify(
-          req.body
-        )}`
-      );
-
-      return res.status(400).json({
-        error: {
-          message: errors.array()[0].msg,
-        },
-      });
-    }
-
-    next();
-  },
+  validateRequestMiddleware,
 ];
 
 export default validateUpdateEnrollmentRequestBodyMiddleware;
