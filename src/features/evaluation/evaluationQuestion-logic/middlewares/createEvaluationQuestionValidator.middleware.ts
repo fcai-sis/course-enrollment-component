f222497c90306f38aaae712b9ad8be23593d6264
Amr Khaled <lewolfje@gmail.com>
2024-06-22 11:44:18 +0300

    fix typos and imports

--- src/features/evaluation/evaluationQuestion-logic/middlewares/createEvaluationQuestionValidator.middleware.ts
+++ src/features/evaluation/evaluationQuestion-logic/middlewares/createEvaluationQuestionValidator.middleware.ts
@@ -1,46 +1,46 @@
 import * as validator from "express-validator";
 import { Request, Response, NextFunction } from "express";
 import logger from "../../../../core/logger";
-import { QuestionTypes } from "features/evaluation/data/enums/questionTypes.enum";
+import { QuestionTypes } from "../../data/enums/questionTypes.enum";
 
 const createEvaluationQuestionValidatorMiddleware = [
-    validator
-        .body("question")
-        .exists()
-        .withMessage("Question is required")
-        .isString()
-        .withMessage("Question must be a string"),
-    validator
-        .body("type")
-        .exists()
-        .withMessage("Type is required")
-        .isString()
-        .withMessage("Type must be a string")
-        .isIn(Object.values(QuestionTypes)),
+  validator
+    .body("question")
+    .exists()
+    .withMessage("Question is required")
+    .isString()
+    .withMessage("Question must be a string"),
+  validator
+    .body("type")
+    .exists()
+    .withMessage("Type is required")
+    .isString()
+    .withMessage("Type must be a string")
+    .isIn(Object.values(QuestionTypes)),
 
-    (req: Request, res: Response, next: NextFunction) => {
-        logger.debug(
-            `Validating create evaluation question validator middleware: ${JSON.stringify(
-                req.body
-            )}`
-        );
-        const errors = validator.validationResult(req);
+  (req: Request, res: Response, next: NextFunction) => {
+    logger.debug(
+      `Validating create evaluation question validator middleware: ${JSON.stringify(
+        req.body
+      )}`
+    );
+    const errors = validator.validationResult(req);
 
-        if (!errors.isEmpty()) {
-            logger.debug(
-                `Validation failed for create evaluation question validator middleware: ${JSON.stringify(
-                    req.body
-                )}`
-            );
+    if (!errors.isEmpty()) {
+      logger.debug(
+        `Validation failed for create evaluation question validator middleware: ${JSON.stringify(
+          req.body
+        )}`
+      );
 
-            return res.status(400).json({
-                error: {
-                    message: errors.array()[0].msg,
-                },
-            });
-        }
-        next();
-    },
+      return res.status(400).json({
+        error: {
+          message: errors.array()[0].msg,
+        },
+      });
+    }
+    next();
+  },
 ];
 
 export default createEvaluationQuestionValidatorMiddleware;
--- src/features/evaluation/evaluationQuestion-logic/middlewares/updateEvaluationQuestionValidator.middleware.ts
+++ src/features/evaluation/evaluationQuestion-logic/middlewares/updateEvaluationQuestionValidator.middleware.ts
@@ -1,46 +1,44 @@
 import * as validator from "express-validator";
 import { Request, Response, NextFunction } from "express";
 import logger from "../../../../core/logger";
-import { QuestionTypes } from "features/evaluation/data/enums/questionTypes.enum";
+import { QuestionTypes } from "../../data/enums/questionTypes.enum";
 
 const updateEvaluationQuestionValidatorMiddleware = [
-    validator
-        .body("question")
-        .optional()
-        .withMessage("Question is required")
-        .isString()
-        .withMessage("Question must be a string"),
-    validator
-        .body("type")
-        .optional()
-        .withMessage("Type is required")
-        .isString()
-        .withMessage("Type must be a string")
-        .isIn(Object.values(QuestionTypes)),
+  validator
+    .body("question")
+    .optional()
+    .isString()
+    .withMessage("Question must be a string"),
+  validator
+    .body("type")
+    .optional()
+    .isString()
+    .withMessage("Type must be a string")
+    .isIn(Object.values(QuestionTypes)),
 
-    (req: Request, res: Response, next: NextFunction) => {
-        logger.debug(
-            `Validating update evaluation question validator middleware: ${JSON.stringify(
-                req.body
-            )}`
-        );
-        const errors = validator.validationResult(req);
+  (req: Request, res: Response, next: NextFunction) => {
+    logger.debug(
+      `Validating update evaluation question validator middleware: ${JSON.stringify(
+        req.body
+      )}`
+    );
+    const errors = validator.validationResult(req);
 
-        if (!errors.isEmpty()) {
-            logger.debug(
-                `Validation failed for update evaluation question validator middleware: ${JSON.stringify(
-                    req.body
-                )}`
-            );
+    if (!errors.isEmpty()) {
+      logger.debug(
+        `Validation failed for update evaluation question validator middleware: ${JSON.stringify(
+          req.body
+        )}`
+      );
 
-            return res.status(400).json({
-                error: {
-                    message: errors.array()[0].msg,
-                },
-            });
-        }
-        next();
-    },
+      return res.status(400).json({
+        error: {
+          message: errors.array()[0].msg,
+        },
+      });
+    }
+    next();
+  },
 ];
 
 export default updateEvaluationQuestionValidatorMiddleware;
