0be6cb07062176ee3147cd36f38f045c41f5d6e0
Amr Khaled <lewolfje@gmail.com>
2024-06-23 22:58:33 +0300

    add route for assigning departments

--- src/features/departmentEnrollment/studentPreference.routes.ts
+++ src/features/departmentEnrollment/studentPreference.routes.ts
@@ -4,6 +4,7 @@ import { asyncHandler } from "@fcai-sis/shared-utilities";
 import validateStudentPreferenceRequestMiddleware from "./logic/middlewares/validateStudentPreferenceRequest.middleware";
 import submitStudentPreferenceHandler from "./logic/handlers/submitStudentPreference.handler";
 import { Role, checkRole } from "@fcai-sis/shared-middlewares";
+import assignDepartmentsBasedOnStudentPreferencesHandler from "./logic/handlers/assignDepartmentsBasedOnPreferences.handler";
 
 const studentPreferenceRoutes = (router: Router) => {
   router.post(
@@ -13,6 +14,11 @@ const studentPreferenceRoutes = (router: Router) => {
     validateStudentPreferenceRequestMiddleware,
 
     asyncHandler(submitStudentPreferenceHandler)
+  );
+  router.post(
+    "/assign",
+
+    asyncHandler(assignDepartmentsBasedOnStudentPreferencesHandler)
   );
 };
 
