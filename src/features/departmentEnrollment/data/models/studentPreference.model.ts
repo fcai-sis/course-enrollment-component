4496d8913d357efef00149b9caa50ac066be4866
Amr Khaled <lewolfje@gmail.com>
2024-06-23 21:26:04 +0300

    refactor model

--- src/features/departmentEnrollment/data/models/studentPreference.model.ts
+++ src/features/departmentEnrollment/data/models/studentPreference.model.ts
@@ -1,14 +1,47 @@
-import mongoose, { Schema, Document } from 'mongoose';
+import { departmentModelName, studentModelName } from "@fcai-sis/shared-models";
+import { ForeignKeyNotFound } from "@fcai-sis/shared-utilities";
+import mongoose, { Schema, Document } from "mongoose";
 
 interface IStudentPreference extends Document {
-  studentId: mongoose.Types.ObjectId;
+  student: mongoose.Types.ObjectId;
   preferences: mongoose.Types.ObjectId[];
 }
 
-const studentPreferenceSchema: Schema = new Schema({
-  studentId: { type: mongoose.Types.ObjectId, ref: 'Student', required: true },
-  preferences: [{ type: mongoose.Types.ObjectId, ref: 'Department', required: true }]
+const studentPreferenceSchema = new mongoose.Schema<IStudentPreference>({
+  student: {
+    type: Schema.Types.ObjectId,
+    ref: studentModelName,
+    required: true,
+  },
+  preferences: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: departmentModelName,
+    },
+  ],
 });
 
-const StudentPreferenceModel = mongoose.model<IStudentPreference>('StudentPreference', studentPreferenceSchema);
-export default StudentPreferenceModel;
+// Pre-save hook to ensure referential integrity
+studentPreferenceSchema.pre("save", async function (next) {
+  try {
+    const departments = await mongoose
+      .model(departmentModelName)
+      .find({ _id: { $in: this.preferences } });
+    if (departments.length !== this.preferences.length) {
+      throw new ForeignKeyNotFound("Some departments not found");
+    }
+
+    next();
+  } catch (error: any) {
+    return next(error);
+  }
+});
+
+export const studentPreferenceModelName = "StudentPreference";
+
+export const studentPreferenceModel =
+  mongoose.models.StudentPreference ||
+  mongoose.model<IStudentPreference>(
+    studentPreferenceModelName,
+    studentPreferenceSchema
+  );
