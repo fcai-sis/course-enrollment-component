ac274f2f4296fd23d8783e4478c72447143b5f3e
Amr Khaled <lewolfje@gmail.com>
2024-06-25 23:15:42 +0300

    add level check before submitting preferences

--- src/features/departmentEnrollment/logic/handlers/submitStudentPreference.handler.ts
+++ src/features/departmentEnrollment/logic/handlers/submitStudentPreference.handler.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express";
 import { studentPreferenceModel } from "../../data/models/studentPreference.model";
 import { TokenPayload } from "@fcai-sis/shared-middlewares";
-import { StudentModel } from "@fcai-sis/shared-models";
+import { AcademicStudentModel, StudentModel } from "@fcai-sis/shared-models";
 
 type HandlerRequest = Request<
   {},
@@ -18,6 +18,27 @@ const handler = async (req: HandlerRequest, res: Response) => {
   const authenticatedStudent = await StudentModel.findOne({
     user: user.userId,
   });
+
+  const academicStudent = await AcademicStudentModel.findOne({
+    student: authenticatedStudent._id,
+  });
+
+  if (!academicStudent) {
+    return res.status(400).json({
+      error: {
+        message: "Student not found",
+      },
+    });
+  }
+  // TODO: bylaw field maybe?
+  if (academicStudent.level < 3) {
+    return res.status(400).json({
+      error: {
+        message:
+          "You must be at least level 3 to submit department preferences",
+      },
+    });
+  }
 
   const studentPreference = new studentPreferenceModel({
     student: authenticatedStudent._id,
