925d1f9f8e86360fd1b065275283ad90c841c7ee
Amr Khaled <lewolfje@gmail.com>
2024-06-26 10:57:59 +0300

    improve validation for student preference request middleware

--- src/features/departmentEnrollment/logic/middlewares/validateStudentPreferenceRequest.middleware.ts
+++ src/features/departmentEnrollment/logic/middlewares/validateStudentPreferenceRequest.middleware.ts
@@ -1,16 +1,28 @@
 import * as validator from "express-validator";
-import { Request, Response, NextFunction } from "express";
 import { validateRequestMiddleware } from "@fcai-sis/shared-middlewares";
+import { DepartmentModel } from "@fcai-sis/shared-models";
 
 const validateStudentPreferenceRequestMiddleware = [
   validator
     .body("preferences")
     .exists()
     .withMessage("Department preferences are required")
-    .isArray({ min: 1 })
+    .isArray()
     .withMessage(
-      "Department preferences must be an array with at least one department ID"
-    ),
+      "Department preferences must be an array of department IDs"
+    )
+    .custom(async (preferences: string[]) => {
+      // make sure the array length is the same as the length of all departments in the database
+      const departments = await DepartmentModel.find({
+        program: "GENERAL",
+      });
+
+      if (departments.length !== preferences.length) {
+        throw new Error("One or more department IDs are invalid or missing");
+      }
+
+      return true;
+    }),
   validator
     .body("preferences.*")
     .isMongoId()
