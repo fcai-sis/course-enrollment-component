cfa58279072fc605fc4118aa32758b04f033b4ca
Amr Khaled <lewolfje@gmail.com>
2024-06-29 19:43:15 +0300

    guard submit

--- src/features/evaluation/evaluationAnswer-logic/evaluationAnswer.routes.ts
+++ src/features/evaluation/evaluationAnswer-logic/evaluationAnswer.routes.ts
@@ -3,11 +3,12 @@ import { Router } from "express";
 import { asyncHandler } from "@fcai-sis/shared-utilities";
 import paginate from "express-paginate";
 import submitEvaluationAnswersHandler from "./handlers/submitEvaluationAnswers.handler";
+import { Role, checkRole } from "@fcai-sis/shared-middlewares";
 
 const evaluationAnswerRoutes = (router: Router) => {
   router.post(
     "/submit-answers",
-
+    checkRole([Role.STUDENT]),
     asyncHandler(submitEvaluationAnswersHandler),
   );
 };
