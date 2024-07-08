d5bd9acbfb49ffba780400306bc0c06567522571
Youssef Atta'i <youssefgalalnazem@gmail.com>
2024-07-08 18:22:11 +0300

    refactor: Update grade-template.ts to use double quotes consistently

--- src/features/utils/grade-template.ts
+++ src/features/utils/grade-template.ts
@@ -1,14 +1,14 @@
-import ExcelJS from 'exceljs';
+import ExcelJS from "exceljs";
 
 export const generateExcelTemplate = async () => {
   const workbook = new ExcelJS.Workbook();
-  const worksheet = workbook.addWorksheet('Grading Template');
+  const worksheet = workbook.addWorksheet("Grading Template");
 
   // Add column headers
   worksheet.columns = [
-    { header: 'studentId', key: 'studentId', width: 20 },
-    { header: 'termWorkMark', key: 'mark', width: 20 },
-    { header: 'finalExamMark', key: 'mark', width: 20}
+    { header: "studentId", key: "studentId", width: 20 },
+    { header: "termWorkMark", key: "mark", width: 20 },
+    { header: "finalExamMark", key: "mark", width: 20 },
   ];
 
   return workbook;
